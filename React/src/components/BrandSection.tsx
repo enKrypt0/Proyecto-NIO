0e83d5e45785655510dedb2a24076dac4318a005
enKrypt0 <bydiego369@gmail.com>
2025-05-21 01:26:56 -0500

    React: Cambios con el background de App y cambio en las marcas Trabajaremos con tipos de licores, en los cuales se puede filtrar por marcas dependiendo del tipo de licor

--- React/src/components/BrandSection.tsx
+++ React/src/components/BrandSection.tsx
@@ -1,11 +1,12 @@
 import '../BrandSection.css';
 
-const brands = ['Todos', 'Ron', 'Tequila', 'Johnnie Walker']; // Se pueden añadir mas, usando esta constante se despliegan automaticamente
+const brands = ['Todos', 'Ron', 'Tequila', 'Whiskey', 'Vodka', 'Ginebra', 'Vermouth', 'Bitter', 'Licores', 'Espumoso', 'Champagne'];
+// Se pueden añadir mas, usando esta constante se despliegan automaticamente
 
 const BrandSection = () => {
   return (
     <section className="brands">
-      <h2>Marcas con las que trabajamos:</h2>
+      <h2>Licores con los que trabajamos:</h2>
       <div className="brand-list">
         {brands.map((brand, index) => (
           <div key={index} className="brand">
